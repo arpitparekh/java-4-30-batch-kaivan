24e4f15aaed4ac639492feda2cdf3c38ffe06d0c
arpitparekh <arpitparekh9@gmail.com>
2024-04-28 12:28:10 +0530

    error ayi ajj

---
+++ oopc/Employee.java
@@ -0,0 +1,22 @@
+package oopc;
+
+public class Employee { // access modifiers  // encapsulation
+    
+    // public // private  // protected
+
+    int id;
+    String name;
+    float height;
+
+    // void setName(String myName) {  // nothing then default
+    //     name = myName;
+    // }
+    
+    // void setHeight(float myHeight) {
+    //     height = myHeight;
+    // }
+    
+
+    // constructor name and class will always be same
+    
+}
---
+++ oopc/OOPCMain.java
@@ -0,0 +1,14 @@
+package oopc;
+
+public class OOPCMain {
+
+    public static void main(String[] args) {
+        
+        Employee e = new Employee();
+        e.id = 23;
+        e.name = "Maulik";
+        e.height = (float) 234.56;
+        
+        System.out.println(e);
+    }
+}
